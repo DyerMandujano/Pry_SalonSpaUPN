86e05244cab60caddfa426a0c9ed5e63fffc1a58
DyerMandujano <dyer0206@gmail.com>
2025-10-06 19:30:29 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pry_Solu_SalonSPA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
